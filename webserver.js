7ef777f855a257b09c790fe3645d38829a758e86
Alex Michelbrink <alexmichelbrink26@gmail.com>
2024-10-11 13:47:05 -0400

    added functionality for mp3s being processed and deleted

--- webserver.js
+++ webserver.js
@@ -11,10 +11,8 @@ var cors = require('cors');
 const { exec } = require("child_process");
 // File System Library
 const fs = require('fs');
-//database functions
+// database functions
 const { getScores } = require('./client/database.js');
-// multer for file uploads
-const multer = require('multer');
 
 //api to get scores using for testing
 app.get('/scores', async (req, res) => {
@@ -31,7 +29,6 @@ app.get('/scores', async (req, res) => {
 var port = 32787;
 if (fs.existsSync(__dirname + "/port.txt")) port = parseInt(fs.readFileSync(__dirname + "/port.txt"));
 
-
 // Sets the Cors access policy
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -46,33 +43,6 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
 
-// storage for uploaded files
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, __dirname + '/tempSongUploads'); // folder to hold mp3s
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname); // ensures use of original file name
-    }
-});
-
-// create multer object
-const upload = multer({ storage: storage });
-
-// upload and process files
-app.post('/newSongUpload', upload.single('newSong'), (req, res) => {
-
-    // run python script (pass file name/location as argument?)
-
-    res.json({ 
-        message: 'File uploaded successfully!',
-        filename: req.file.originalname 
-    });
-
-    // delete uploaded mp3
-
-});
-
 // Allow a folder to be viewed in the web-server
 // All files in the "/client/assets" folder will be vieable to the client
 app.use('/assets', express.static(__dirname + '/client/assets'));
@@ -85,6 +55,59 @@ app.get('*', function (req, res) {
 // start the server
 serv.listen(port);
 console.log(`Server started on: http://localhost:${port}`);
+
+
+// FILE UPLOADS ---------------------------------
+
+// multer for file uploads
+const multer = require('multer');
+// require for running python processing script
+const { spawn } = require("child_process");
+
+//storage for uploaded files
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, __dirname + '/client/assets/game/keyboardhero/levels'); // folder to hold mp3s
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname); // ensures use of original file name
+    }
+});
+
+// create multer object
+const upload = multer({ storage: storage });
+
+// upload and process files
+app.post('/newSongUpload', upload.single('newSong'), (req, res) => {
+
+    // location of where levels are stored
+    const levelLocation = __dirname + "/client/assets/game/keyboardhero/levels/";
+
+    const songFilePath = levelLocation + req.file.originalname; // final mp3 file path
+    const songName = req.file.originalname; // original mp3 name
+
+    // launch python process with song file path and original mp3 name
+    const pythonProcess = spawn("python", [__dirname + "/beatmapGenerator.py", songFilePath, songName]);
+
+    // delete original mp3 when python process closes
+    pythonProcess.on('close', (code) => {
+        fs.unlink(songFilePath, (err) => {
+            if (err) {
+                console.error('ORIGINAL MP3 DELETION ERROR:', err);
+            } else {
+                console.log('MP3 DELETED SUCCESSFULLY.');
+            }
+        });
+    })
+
+    // send info back to webpage
+    res.json({ 
+        message: 'File uploaded successfully!',
+        filename: req.file.originalname 
+    });
+});
+
+// FILE UPLOADS END -----------------------------
 
 
 // Below will be the code for multiplayer and score system. 
