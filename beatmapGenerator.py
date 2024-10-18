fe408a248ae83a743567a05abaed652d70613d93
Alex Michelbrink <alexmichelbrink26@gmail.com>
2024-10-18 15:33:13 -0400

    added buffer between notes

--- beatmapGenerator.py
+++ beatmapGenerator.py
@@ -12,32 +12,44 @@ units = 0 # seconds
 
 # get info from audio file ========================================================================
 
-# get song title
+# get song location and title
 songLocation = sys.argv[1]
 songName = sys.argv[2]
 
 # load in the audio file
 y, sr = librosa.load(songLocation)
 
-# get timestamps (in seconds) of peaks in amplitude 
-onset_times = librosa.onset.onset_detect(y=y, sr=sr, units='time')
-timestamps = onset_times.tolist() # convert to numpy array to list
+# get peak strengths throughout whole mp3
+onset_strengths = librosa.onset.onset_strength(y=y,sr=sr)
+# get timestamps of peaks
+onset_times = librosa.onset.onset_detect(y=y,sr=sr,onset_envelope=onset_strengths,delta=0.03,backtrack=True)
+# get strength of peaks at peak times
+timeStrengths = onset_strengths[onset_times]
 
-# find the amplitudes of each timestamp
-amps = []
-for t in onset_times:
-    sample_index = int(t * sr) # convert timestamp to sample idex
-    amps.append([t.item(),y[sample_index].item()]) # add timestamp and its amplitude
+onset_times = librosa.frames_to_time(onset_times, sr=sr) # convert to timestamps
+timestamps = []
+for i, (onset_times,timeStrengths) in enumerate(zip(onset_times,timeStrengths)):
+    timestamps.append([onset_times.item(),timeStrengths.item(),1])
 
-# get range of amplitudes
-min = 10
-max = -10
-for t in amps:
+# get range of strengths
+min = 100
+max = -1
+for t in timestamps:
     if (t[1] < min):
         min = t[1]
     if (t[1] > max):
         max = t[1]
-range = max - min
+strengthRange = max - min
+
+bufferTime = 0.25 # minimum time (seconds) between notes
+n = 1
+c = 0
+while (n < len(timestamps)):
+    if (timestamps[n][0] - timestamps[c][0] < bufferTime):
+        timestamps[n][2] = 0
+    else:
+        c = n
+    n = n + 1
 
 # assign timestamps to lanes ======================================================================
 
@@ -49,17 +61,18 @@ track3 = []
 tracks = [track0, track1, track2, track3]
 
 # assign timestamps to lanes based on amplitude
-for t in amps:
-    if (t[1] > (min + range*.9)):
-        track0.append(t[0])
-        track1.append(t[0])
-        track2.append(t[0])
-        track3.append(t[0])
-    elif (t[1] > (min + range*.7)):
-        track1.append(t[0])
-        track2.append(t[0])
-    else:
-        (tracks[random.randint(0,3)]).append(t[0])
+for t in timestamps:
+    if (t[2] == 1):
+        if (t[1] > (min + strengthRange*.8)):
+            track0.append(t[0])
+            track1.append(t[0])
+            track2.append(t[0])
+            track3.append(t[0])
+        elif (t[1] > (min + strengthRange*.5)):
+            track1.append(t[0])
+            track2.append(t[0])
+        else:
+            (tracks[random.randint(0,3)]).append(t[0])
 
 
 # get base64 data  ===============================================================================
