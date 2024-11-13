0de31c07493ba48f03e9f8be8b5a96e0c82218bd
Alex Michelbrink <alexmichelbrink26@gmail.com>
2024-11-13 10:46:45 -0500

    fixed basic upload page functionality, updated singleplayer variables

--- beatmapGenerator.py
+++ beatmapGenerator.py
@@ -41,14 +41,9 @@ y, sr = librosa.load(songLocation)
 #D_harmonic, D_percussive = librosa.decompose.hpss(librosa.stft(y))
 #y = librosa.istft(D_percussive, length=len(y))
 
-# focus on percussive part of song (better for beat detection)
-#D_harmonic, D_percussive = librosa.decompose.hpss(librosa.stft(y))
-#y = librosa.istft(D_percussive, length=len(y))
-
 # get onset/peak strengths throughout whole mp3
 onsetStrengths = librosa.onset.onset_strength(y=y,sr=sr)
 # get timestamps of beats
-tempo, beats = librosa.beat.beat_track(y=y,sr=sr,onset_envelope=onsetStrengths,tightness=0.01,trim=True)
 tempo, beats = librosa.beat.beat_track(y=y,sr=sr,onset_envelope=onsetStrengths,tightness=0.01,trim=True)
 # get onset strength of gathered beats
 beatStrengths = onsetStrengths[beats]
@@ -57,10 +52,9 @@ beats = librosa.frames_to_time(beats, sr=sr) # convert frames to seconds
 timestamps = [] # to hold timestamp information
 # iterate through beats and their strengths
 for i, (beats,beatStrengths) in enumerate(zip(beats,beatStrengths)): 
-    silenceThreshold = 2.5 # exclude beats below a certain strength (silent moments)
+    silenceThreshold = 1.5
     if (beatStrengths > silenceThreshold):
         # beat timestamp, beat strength, include marker, and time till next beat
-        timestamps.append([beats.item()-0.05,beatStrengths.item(),1,0])
         timestamps.append([beats.item()-0.05,beatStrengths.item(),1,0])
 
 n = 1 # index of next beat
@@ -77,11 +71,10 @@ while (n < len(timestamps)):
 n = 1 # index of next beat
 c = 0 # index of current beat
 while (n < len(timestamps)):
-    if (timestamps[n][1] < (timestamps[c][1] - 0.25) and timestamps[c][3] < 0.25 and timestamps[c][2] == 1):
+    if (timestamps[n][1] < (timestamps[c][1] - 0.5) and timestamps[c][3] < 0.25 and timestamps[c][2] == 1):
         timestamps[n][2] = 0
     n = n + 1
     c = c + 1
-
 
 # get identifier ======================================================================
 
@@ -105,6 +98,8 @@ tracks = [track0, track1, track2, track3]
 # assign timestamps to lanes based on amplitude
 pattern = 0
 currentGap = timestamps[0][3]
+median = statistics.median([t[1] for t in timestamps]) # get median of strength of beats
+median = np.percentile([t[1] for t in timestamps],75)
 topBeats = np.percentile([t[1] for t in timestamps],98)
 
 if (difficulty == 1):
@@ -119,6 +114,7 @@ for t in timestamps:
     if (t[2] == 1): # only consider included beats
 
         # change patterns if not similar gap between notes or there's a significant strength to the beat
+        if ((not (t[3] >= currentGap-0.10 and t[3] <= currentGap+0.10)) or t[1] > median or pattern == 7):
         if ((not (t[3] >= currentGap-0.10 and t[3] <= currentGap+0.10)) or t[1] > median or pattern == 7):
 
             # make sure pattern changes
