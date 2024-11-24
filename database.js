eb5eb994f9c18b001c75fe623b47ae74f9eee324
jkriz2 <161981523+jkriz2@users.noreply.github.com>
2024-11-24 17:15:10 -0500

    forgot to revert testing settings

--- database.js
+++ database.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'alphateam',
-    database: 'keyboard_hero',
-    password: 'AlphaTeam',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
 });
 
 // Use the `promise()` function of the pool for promise-based queries
